3056c6c83effc7f9b05eaf5075a419fe7ed3e721
░▒▓█│Walkman│█▓▒░ <matthewcarter17@gmail.com>
2014-05-20 00:43:17 +0200

    Version 1.0 code done
    all the code has been done. version 1.1 will either have advanced
    options or a notification icon, depending on what I feel like doing.

--- YTVL/YTVL.Designer.vb
+++ YTVL/YTVL.Designer.vb
@@ -24,25 +24,26 @@ Partial Class YTVL
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(YTVL))
         Me.grpBoxInputs = New System.Windows.Forms.GroupBox()
-        Me.grpBoxOutputs = New System.Windows.Forms.GroupBox()
-        Me.grpBoxAbout = New System.Windows.Forms.GroupBox()
-        Me.txtComboVID = New System.Windows.Forms.ComboBox()
-        Me.txtTime = New System.Windows.Forms.TextBox()
-        Me.chkFeather = New System.Windows.Forms.CheckBox()
-        Me.lblFeather = New System.Windows.Forms.LinkLabel()
-        Me.chkHL = New System.Windows.Forms.CheckBox()
-        Me.lblHL = New System.Windows.Forms.Label()
+        Me.btnAdvanced = New System.Windows.Forms.Button()
         Me.txtList = New System.Windows.Forms.TextBox()
-        Me.btnVideo = New System.Windows.Forms.Button()
-        Me.btnComments = New System.Windows.Forms.Button()
-        Me.btnVideoInfo = New System.Windows.Forms.Button()
-        Me.btnEmbed = New System.Windows.Forms.Button()
+        Me.lblHL = New System.Windows.Forms.Label()
+        Me.chkHL = New System.Windows.Forms.CheckBox()
+        Me.lblFeather = New System.Windows.Forms.LinkLabel()
+        Me.chkFeather = New System.Windows.Forms.CheckBox()
+        Me.txtTime = New System.Windows.Forms.TextBox()
+        Me.txtComboVID = New System.Windows.Forms.ComboBox()
+        Me.grpBoxOutputs = New System.Windows.Forms.GroupBox()
         Me.btnReset = New System.Windows.Forms.Button()
-        Me.lnkOriginalPage = New System.Windows.Forms.LinkLabel()
-        Me.lnkSourceCode = New System.Windows.Forms.LinkLabel()
-        Me.lnkReportProblem = New System.Windows.Forms.LinkLabel()
+        Me.btnEmbed = New System.Windows.Forms.Button()
+        Me.btnVideoInfo = New System.Windows.Forms.Button()
+        Me.btnComments = New System.Windows.Forms.Button()
+        Me.btnVideo = New System.Windows.Forms.Button()
+        Me.grpBoxAbout = New System.Windows.Forms.GroupBox()
+        Me.lnkWalkman = New System.Windows.Forms.LinkLabel()
         Me.lnkReleases = New System.Windows.Forms.LinkLabel()
-        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.lnkReportProblem = New System.Windows.Forms.LinkLabel()
+        Me.lnkSourceCode = New System.Windows.Forms.LinkLabel()
+        Me.lnkOriginalPage = New System.Windows.Forms.LinkLabel()
         Me.grpBoxInputs.SuspendLayout()
         Me.grpBoxOutputs.SuspendLayout()
         Me.grpBoxAbout.SuspendLayout()
@@ -51,6 +52,7 @@ Partial Class YTVL
         'grpBoxInputs
         '
         resources.ApplyResources(Me.grpBoxInputs, "grpBoxInputs")
+        Me.grpBoxInputs.Controls.Add(Me.btnAdvanced)
         Me.grpBoxInputs.Controls.Add(Me.txtList)
         Me.grpBoxInputs.Controls.Add(Me.lblHL)
         Me.grpBoxInputs.Controls.Add(Me.chkHL)
@@ -60,6 +62,56 @@ Partial Class YTVL
         Me.grpBoxInputs.Controls.Add(Me.txtComboVID)
         Me.grpBoxInputs.Name = "grpBoxInputs"
         Me.grpBoxInputs.TabStop = False
+        '
+        'btnAdvanced
+        '
+        resources.ApplyResources(Me.btnAdvanced, "btnAdvanced")
+        Me.btnAdvanced.Name = "btnAdvanced"
+        Me.btnAdvanced.UseVisualStyleBackColor = True
+        '
+        'txtList
+        '
+        Me.txtList.AllowDrop = True
+        resources.ApplyResources(Me.txtList, "txtList")
+        Me.txtList.Name = "txtList"
+        '
+        'lblHL
+        '
+        resources.ApplyResources(Me.lblHL, "lblHL")
+        Me.lblHL.Name = "lblHL"
+        '
+        'chkHL
+        '
+        resources.ApplyResources(Me.chkHL, "chkHL")
+        Me.chkHL.Name = "chkHL"
+        Me.chkHL.UseVisualStyleBackColor = True
+        '
+        'lblFeather
+        '
+        resources.ApplyResources(Me.lblFeather, "lblFeather")
+        Me.lblFeather.Name = "lblFeather"
+        Me.lblFeather.TabStop = True
+        Me.lblFeather.UseCompatibleTextRendering = True
+        '
+        'chkFeather
+        '
+        resources.ApplyResources(Me.chkFeather, "chkFeather")
+        Me.chkFeather.Name = "chkFeather"
+        Me.chkFeather.UseVisualStyleBackColor = True
+        '
+        'txtTime
+        '
+        Me.txtTime.AllowDrop = True
+        resources.ApplyResources(Me.txtTime, "txtTime")
+        Me.txtTime.Name = "txtTime"
+        '
+        'txtComboVID
+        '
+        Me.txtComboVID.AllowDrop = True
+        Me.txtComboVID.FormattingEnabled = True
+        Me.txtComboVID.Items.AddRange(New Object() {resources.GetString("txtComboVID.Items"), resources.GetString("txtComboVID.Items1")})
+        resources.ApplyResources(Me.txtComboVID, "txtComboVID")
+        Me.txtComboVID.Name = "txtComboVID"
         '
         'grpBoxOutputs
         '
@@ -72,74 +124,11 @@ Partial Class YTVL
         Me.grpBoxOutputs.Name = "grpBoxOutputs"
         Me.grpBoxOutputs.TabStop = False
         '
-        'grpBoxAbout
+        'btnReset
         '
-        resources.ApplyResources(Me.grpBoxAbout, "grpBoxAbout")
-        Me.grpBoxAbout.Controls.Add(Me.LinkLabel1)
-        Me.grpBoxAbout.Controls.Add(Me.lnkReleases)
-        Me.grpBoxAbout.Controls.Add(Me.lnkReportProblem)
-        Me.grpBoxAbout.Controls.Add(Me.lnkSourceCode)
-        Me.grpBoxAbout.Controls.Add(Me.lnkOriginalPage)
-        Me.grpBoxAbout.Name = "grpBoxAbout"
-        Me.grpBoxAbout.TabStop = False
-        '
-        'txtComboVID
-        '
-        Me.txtComboVID.FormattingEnabled = True
-        resources.ApplyResources(Me.txtComboVID, "txtComboVID")
-        Me.txtComboVID.Name = "txtComboVID"
-        '
-        'txtTime
-        '
-        resources.ApplyResources(Me.txtTime, "txtTime")
-        Me.txtTime.Name = "txtTime"
-        '
-        'chkFeather
-        '
-        resources.ApplyResources(Me.chkFeather, "chkFeather")
-        Me.chkFeather.Name = "chkFeather"
-        Me.chkFeather.UseVisualStyleBackColor = True
-        '
-        'lblFeather
-        '
-        resources.ApplyResources(Me.lblFeather, "lblFeather")
-        Me.lblFeather.Name = "lblFeather"
-        Me.lblFeather.TabStop = True
-        Me.lblFeather.UseCompatibleTextRendering = True
-        '
-        'chkHL
-        '
-        resources.ApplyResources(Me.chkHL, "chkHL")
-        Me.chkHL.Name = "chkHL"
-        Me.chkHL.UseVisualStyleBackColor = True
-        '
-        'lblHL
-        '
-        resources.ApplyResources(Me.lblHL, "lblHL")
-        Me.lblHL.Name = "lblHL"
-        '
-        'txtList
-        '
-        resources.ApplyResources(Me.txtList, "txtList")
-        Me.txtList.Name = "txtList"
-        '
-        'btnVideo
-        '
-        resources.ApplyResources(Me.btnVideo, "btnVideo")
-        Me.btnVideo.Name = "btnVideo"
-        Me.btnVideo.UseVisualStyleBackColor = True
-        '
-        'btnComments
-        '
-        resources.ApplyResources(Me.btnComments, "btnComments")
-        Me.btnComments.Name = "btnComments"
-        Me.btnComments.UseVisualStyleBackColor = True
-        '
-        'btnVideoInfo
-        '
-        resources.ApplyResources(Me.btnVideoInfo, "btnVideoInfo")
-        Me.btnVideoInfo.Name = "btnVideoInfo"
-        Me.btnVideoInfo.UseVisualStyleBackColor = True
+        resources.ApplyResources(Me.btnReset, "btnReset")
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.UseVisualStyleBackColor = True
         '
         'btnEmbed
         '
@@ -147,29 +136,42 @@ Partial Class YTVL
         Me.btnEmbed.Name = "btnEmbed"
         Me.btnEmbed.UseVisualStyleBackColor = True
         '
-        'btnReset
+        'btnVideoInfo
         '
-        resources.ApplyResources(Me.btnReset, "btnReset")
-        Me.btnReset.Name = "btnReset"
-        Me.btnReset.UseVisualStyleBackColor = True
+        resources.ApplyResources(Me.btnVideoInfo, "btnVideoInfo")
+        Me.btnVideoInfo.Name = "btnVideoInfo"
+        Me.btnVideoInfo.UseVisualStyleBackColor = True
         '
-        'lnkOriginalPage
+        'btnComments
         '
-        resources.ApplyResources(Me.lnkOriginalPage, "lnkOriginalPage")
-        Me.lnkOriginalPage.Name = "lnkOriginalPage"
-        Me.lnkOriginalPage.TabStop = True
+        resources.ApplyResources(Me.btnComments, "btnComments")
+        Me.btnComments.Name = "btnComments"
+        Me.btnComments.UseVisualStyleBackColor = True
         '
-        'lnkSourceCode
+        'btnVideo
         '
-        resources.ApplyResources(Me.lnkSourceCode, "lnkSourceCode")
-        Me.lnkSourceCode.Name = "lnkSourceCode"
-        Me.lnkSourceCode.TabStop = True
+        resources.ApplyResources(Me.btnVideo, "btnVideo")
+        Me.btnVideo.Name = "btnVideo"
+        Me.btnVideo.UseVisualStyleBackColor = True
         '
-        'lnkReportProblem
+        'grpBoxAbout
         '
-        resources.ApplyResources(Me.lnkReportProblem, "lnkReportProblem")
-        Me.lnkReportProblem.Name = "lnkReportProblem"
-        Me.lnkReportProblem.TabStop = True
+        resources.ApplyResources(Me.grpBoxAbout, "grpBoxAbout")
+        Me.grpBoxAbout.Controls.Add(Me.lnkWalkman)
+        Me.grpBoxAbout.Controls.Add(Me.lnkReleases)
+        Me.grpBoxAbout.Controls.Add(Me.lnkReportProblem)
+        Me.grpBoxAbout.Controls.Add(Me.lnkSourceCode)
+        Me.grpBoxAbout.Controls.Add(Me.lnkOriginalPage)
+        Me.grpBoxAbout.Name = "grpBoxAbout"
+        Me.grpBoxAbout.TabStop = False
+        '
+        'lnkWalkman
+        '
+        resources.ApplyResources(Me.lnkWalkman, "lnkWalkman")
+        Me.lnkWalkman.LinkColor = System.Drawing.Color.Silver
+        Me.lnkWalkman.Name = "lnkWalkman"
+        Me.lnkWalkman.TabStop = True
+        Me.lnkWalkman.VisitedLinkColor = System.Drawing.Color.Gray
         '
         'lnkReleases
         '
@@ -177,13 +179,23 @@ Partial Class YTVL
         Me.lnkReleases.Name = "lnkReleases"
         Me.lnkReleases.TabStop = True
         '
-        'LinkLabel1
+        'lnkReportProblem
         '
-        resources.ApplyResources(Me.LinkLabel1, "LinkLabel1")
-        Me.LinkLabel1.LinkColor = System.Drawing.Color.Silver
-        Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.TabStop = True
-        Me.LinkLabel1.VisitedLinkColor = System.Drawing.Color.Gray
+        resources.ApplyResources(Me.lnkReportProblem, "lnkReportProblem")
+        Me.lnkReportProblem.Name = "lnkReportProblem"
+        Me.lnkReportProblem.TabStop = True
+        '
+        'lnkSourceCode
+        '
+        resources.ApplyResources(Me.lnkSourceCode, "lnkSourceCode")
+        Me.lnkSourceCode.Name = "lnkSourceCode"
+        Me.lnkSourceCode.TabStop = True
+        '
+        'lnkOriginalPage
+        '
+        resources.ApplyResources(Me.lnkOriginalPage, "lnkOriginalPage")
+        Me.lnkOriginalPage.Name = "lnkOriginalPage"
+        Me.lnkOriginalPage.TabStop = True
         '
         'YTVL
         '
@@ -223,6 +235,7 @@ Partial Class YTVL
     Friend WithEvents lnkReportProblem As System.Windows.Forms.LinkLabel
     Friend WithEvents lnkSourceCode As System.Windows.Forms.LinkLabel
     Friend WithEvents lnkOriginalPage As System.Windows.Forms.LinkLabel
-    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents lnkWalkman As System.Windows.Forms.LinkLabel
+    Friend WithEvents btnAdvanced As System.Windows.Forms.Button
 
 End Class
--- YTVL/YTVL.vb
+++ YTVL/YTVL.vb
@@ -1,3 +1,137 @@
 ﻿Public Class YTVL
+    Dim Vars As String
 
+    'Buttons
+
+    Private Sub BuildVars()
+        Vars = txtComboVID.Text
+        If chkFeather.Checked = True Then
+            Vars = Vars & "&noFeather=True"
+        End If
+        If chkHL.Checked = True Then
+            Vars = Vars & "&hl=en"
+        End If
+        If txtTime.Text <> "" And txtTime.Text <> "Time" Then
+            Vars = Vars & "&t=" & txtTime.Text
+        End If
+        If txtList.Text <> "" And txtList.Text <> "List" Then
+            Vars = Vars & "&list=" & txtList.Text
+        End If
+    End Sub
+    Private Sub OpenVideo(sender As Object, e As EventArgs) Handles btnVideo.Click
+        BuildVars()
+        Process.Start("http://www.youtube.com/watch?v=" & Vars)
+    End Sub
+
+    Private Sub OpenComments(sender As Object, e As EventArgs) Handles btnComments.Click
+        BuildVars()
+        Process.Start("http://www.youtube.com/all_comments?v=" & Vars)
+    End Sub
+
+    Private Sub OpenVideoInfo(sender As Object, e As EventArgs) Handles btnVideoInfo.Click
+        BuildVars()
+        Process.Start("http://www.youtube.com/get_video_info?v=" & Vars)
+    End Sub
+
+    Private Sub OpenEmbeddedObject(sender As Object, e As EventArgs) Handles btnEmbed.Click
+        BuildVars()
+        Process.Start("http://www.youtube.com/embed/" & Vars)
+    End Sub
+
+    Private Sub ResetForm(sender As Object, e As EventArgs) Handles btnReset.Click
+        txtComboVID.Text = "Video ID"
+        txtTime.Text = "Time"
+        txtList.Text = "List"
+        chkFeather.Checked = False
+        chkHL.Checked = False
+        Me.Height = 230
+        Me.Width = 506
+        btnAdvanced.Text = "More ↓"
+    End Sub
+
+    Private Sub MoreLess(sender As Object, e As EventArgs) Handles btnAdvanced.Click
+        If Me.Height = 400 Then
+            Me.Height = 230
+            btnAdvanced.Text = "More ↓"
+        Else
+            Me.Height = 400
+            btnAdvanced.Text = "Less ↑"
+        End If
+    End Sub
+
+    'Links
+
+    Private Sub OpenFeatherLink(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lblFeather.LinkClicked
+        Process.Start("https://www.youtube.com/testtube")
+    End Sub
+
+    Private Sub OpenOriginalPage(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkOriginalPage.LinkClicked
+        Process.Start("http://walkman100.github.io/Walkman/youtube-video-stuff.htm")
+    End Sub
+
+    Private Sub OpenSourceCode(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkSourceCode.LinkClicked
+        Process.Start("https://github.com/Walkman100/YTVL/")
+    End Sub
+
+    Private Sub ReportProblem(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkReportProblem.LinkClicked
+        Process.Start("https://github.com/Walkman100/YTVL/issues/new")
+    End Sub
+
+    Private Sub OpenReleases(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkReleases.LinkClicked
+        Process.Start("https://github.com/Walkman100/YTVL/releases/latest")
+    End Sub
+
+    Private Sub OpenWalkmanPage(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkWalkman.LinkClicked
+        Process.Start("http://twitter.com/Walkman100")
+    End Sub
+
+    'Text control
+
+    Private Sub txtComboVID_Enter(sender As Object, e As EventArgs) Handles txtComboVID.Click
+        If txtComboVID.Text = "Video ID" Then
+            txtComboVID.Text = ""
+        Else
+            txtComboVID.SelectAll()
+        End If
+    End Sub
+
+    Private Sub txtComboVID_Leave(sender As Object, e As EventArgs) Handles txtComboVID.Leave
+        If txtComboVID.Text = "" Then
+            txtComboVID.Text = "Video ID"
+        ElseIf txtComboVID.Text <> "Video ID" Then
+            If txtComboVID.Items.Contains(txtComboVID.Text) Then
+
+            Else
+                txtComboVID.Items.Add(txtComboVID.Text)
+            End If
+        End If
+    End Sub
+
+    Private Sub txtTime_Enter(sender As Object, e As EventArgs) Handles txtTime.Click
+        If txtTime.Text = "Time" Then
+            txtTime.Text = ""
+        Else
+            txtTime.SelectAll()
+        End If
+    End Sub
+
+    Private Sub txtTime_Leave(sender As Object, e As EventArgs) Handles txtTime.Leave
+        If txtTime.Text = "" Then
+            txtTime.Text = "Time"
+        End If
+    End Sub
+
+    Private Sub txtList_Enter(sender As Object, e As EventArgs) Handles txtList.Click
+        If txtList.Text = "List" Then
+            txtList.Text = ""
+        Else
+            txtList.SelectAll()
+        End If
+    End Sub
+
+    Private Sub txtList_Leave(sender As Object, e As EventArgs) Handles txtList.Leave
+        If txtList.Text = "" Then
+            txtList.Text = "List"
+        End If
+    End Sub
 End Class
