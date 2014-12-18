0bfa5fdc267e02af62683e4aa89254b0f2a26e74
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2014-12-18 02:07:36 +0200

    [Compiler] Patch to allow people who still use spaces to compile

--- Compile.bat
+++ Compile.bat
@@ -1,8 +1,8 @@
 @echo off
 
-%ProgramFiles%\MSBuild\12.0\bin\msbuild.exe /property:Configuration=Release YTVL.sln
+"%ProgramFiles%\MSBuild\12.0\bin\msbuild.exe" /property:Configuration=Release YTVL.sln
 
-%ProgramFiles%\NSIS\makensis.exe "NSIS Installer for YTVL.nsi"
+"%ProgramFiles%\NSIS\makensis.exe" "NSIS Installer for YTVL.nsi"
 del bin\Release\YTVL-Portable.exe
 ren bin\Release\YTVL.exe YTVL-Portable.exe
 
